059c4f3f60ceff13fc3508f48eddb8d0da642109
u-u2 <u-u2@outlook.jp>
2023-09-25 18:32:25 +0900

    move src

--- uu2/install.sh
+++ uu2/install.sh
@@ -1,51 +1,9 @@
 #!/bin/bash
 
 readonly CURRENT_DIR=$PWD
-readonly BACKUP_DIR="$HOME/dotfiles-wsl-backup/"
-
-# source "scripts/apt.sh"
-
-if [ ! -d $BACKUP_DIR ]; then
-    mkdir "$BACKUP_DIR"
-    echo "create backup directory: $BACKUP_DIR"
-fi
-
-echo_indent() {
-    echo $1 | sed 's/^/  /'
-}
-
-has_parent_directory() {
-    [ $(dirname $1) != '.' ]
-}
-
-create_home_path() {
-    if has_parent_directory $1; then
-        echo "$HOME/$(dirname $1)/$(basename $1)"
-    else
-        echo "$HOME/$(basename $1)"
-    fi
-}
 
 cd $(dirname $0)
-for source in $(find virtualhome -type f | sort); do
-    echo "create symlink for $source"
-    relativeSource=$(echo $source | cut -d / -f 2-)
-    destFile=$(create_home_path $relativeSource)
-    if [ -f $fullpath ]; then
-        destBackupDir=$(dirname "$BACKUP_DIR/$relativeSource")
-        if has_parent_directory $relativeSource && [ ! -d $destBackupDir ]; then
-            echo_indent "create backup directory: $destBackupDir"
-            mkdir -p $destBackupDir
-        fi
-        destBackupFile="$destBackupDir/$(basename $source)"
-        echo_indent "backup: $destFile -> $destBackupFile"
-        cp -L $destFile $destBackupFile
-    fi
-    fullSource="$PWD/$source"
-    echo_indent "link: $destFile -> $fullSource"
-    ln -sf $fullSource $destFile
-    echo -e '\n'
-done
+source "scripts/apt.sh"
+source "scripts/symlink.sh"
 
-echo 'done'
 cd $CURRENT_DIR
