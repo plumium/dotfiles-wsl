699cc4a7c323ff2f8be62dcf5d1d748a221a0832
u-u2 <u-u2@outlook.jp>
2023-10-09 02:13:59 +0900

    Add asdf initialization

--- uu2/init/clone.sh
+++ uu2/init/clone.sh
@@ -3,4 +3,6 @@
 if [ ! -d ~/.asdf ]; then
     echo 'install asdf'
     git clone --branch v0.13.1 https://github.com/asdf-vm/asdf.git ~/.asdf
+    . "$HOME/.asdf/asdf.sh"
+    . "$HOME/.asdf/completions/asdf.bash"
 fi
