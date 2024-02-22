77496b6c4c858850a5c143575448d63103f3f858
plumium <plumium@outlook.com>
2024-02-22 14:07:46 +0900

    Change directory name from init to scripts

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ function run() {
 }
 
 cd $(dirname $0)
-run 'init/apt.sh'
-run 'init/clone.sh'
-run 'init/asdf.sh'
-run 'init/symlink.sh'
+run 'scripts/apt.sh'
+run 'scripts/clone.sh'
+run 'scripts/asdf.sh'
+run 'scripts/symlink.sh'
 
 cd $CURRENT_DIR
